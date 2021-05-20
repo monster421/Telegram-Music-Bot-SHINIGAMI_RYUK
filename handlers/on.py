e9ec128cc1eaa3c9db741b26d5ce4c3f819af13a
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-20 21:26:27 +0530

    Update on.py

--- handlers/on.py
+++ handlers/on.py
@@ -22,12 +22,12 @@ from handlers.ryuk.shinigami import DELETE_DELAY
 
 @Client.on_message(main_filter
                    & self_or_contact_filter
-                   & filters.regex("!on$"))
-async def join_group_call(client, m: Message):
+                   & filters.regex("^!on$"))
+async def join_group_call(client, Message):
     group_call = mp.group_call
     group_call.client = client
     if group_call.is_connected:
-        await m.reply_text(f="一═デ︻ **ֆɦɨռɨɢǟʍɨ_Rʏʊӄ** ︻デ═一\n[🦋](https://telegra.ph/file/8bdbb1581cc0914586fe2.jpg)[🦋]**Already joined**")
+        await Message.reply_text(f="一═デ︻ **ֆɦɨռɨɢǟʍɨ_Rʏʊӄ** ︻デ═一\n[🦋](https://telegra.ph/file/8bdbb1581cc0914586fe2.jpg)[🦋]**Already joined**")
         return
-    await group_call.start(m.chat.id)
-    await m.delete()
+    await group_call.start(Message.chat.id)
+    await Message.delete()
