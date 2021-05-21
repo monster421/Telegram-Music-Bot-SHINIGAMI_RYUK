c8d7d7d03a0ec8d506c7d099ad747bf0b02d5ca9
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-21 12:58:03 +0530

    Delete KIRA directory

--- KIRA/pyro_sesion.py
+++
@@ -1,9 +0,0 @@
-"""
-<--------------------------->
-|  ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ     -_-
-<--------------------------->
-
-Remastered Version of Riyuk_Singer_Vrtx
-"""
-
-FOLLOW_README_OR_GET_FROM = "https://replit.com/@mastermindvrtx/ShinigamiRyukPyrogramSesion#main.py"
