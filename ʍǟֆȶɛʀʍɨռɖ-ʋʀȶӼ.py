48d0790dcc02ee123e0df0623b029b7934b2c5a9
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-21 13:24:41 +0530

    Update ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ.py

--- ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ.py
+++ ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ.py
@@ -10,17 +10,14 @@
 "+|========================================== ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ -_- ==============================================+"
 
 from pyrogram import Client as mip, idle
-#from KIRA.pyro_auth import Li
+from KIRA.pyro_auth import Li
 from NoteBook.design_i import *
 
 "+|========================================== ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ -_- ==============================================+"
 
-#API_ID = Li.API_ID
-#API_HASH = Li.API_HASH
-#SESSION_NAME = Li.SESSION_NAME
-API_ID =2299169
-API_HASH = "fc2d0ee997dbdd76eaa0d76a8632c709"
-SESSION_NAME = "BQBGOKWqSdUGQCFw41458h0fpavE3EHmeehmjSynhikSN_-9E5X139lkBx2heTUKkhGsZ_ywYbDH2245s0-zLFFNUPC_DWNatb7my7C15PCMAcW37yTlrPIFYfpw9d8YujfONwVgNT1LNtOhLFAFvTTWrum_DqWNvYIV8qWqf-IDJQIA6GurfzFexv0qB4w7ppkgYHoAwe9K-tXx49bamM43JlLOPgzSM2tLsB1i8lTB_IUNl_nSld12hepAn9JfNn87my0k_5tBBCvD7UY3qYBMHKA277LxpaPw03_fPOgxg_ZO_OJi6oPeevxWh21jDHRcnumQ37Fsm5Q6fGL5UFAla6gsLwA"
+API_ID = Li.API_ID
+API_HASH = Li.API_HASH
+SESSION_NAME = Li.SESSION_NAME
 
 "+|========================================== ʍǟֆȶɛʀʍɨռɖ-ʋʀȶӼ -_- ==============================================+"
 
